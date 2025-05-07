7ba3f8c63a3915d54dfab996d7d377bb3f0f325d
Mo24-coding <mohammedbenaddi@Mohammeds-MacBook-Air.local>
2025-05-07 06:13:07 +0200

    c

--- src/Competitionresult.java
+++ src/Competitionresult.java
@@ -1,2 +1,18 @@
 public class Competitionresult {
+    private String disciplin;
+    private double tid;
+    private String stævne;
+    private int placering;
+    private String dato;
+    private int medlemsnummer;
+
+    public Competitionresult (String disciplin, double tid, String dato, int medlemsnummer){
+        this.disciplin = disciplin;
+        this.tid = tid;
+        this.dato = dato;
+        this.medlemsnummer = medlemsnummer;
+
+    }
+
+
 }
