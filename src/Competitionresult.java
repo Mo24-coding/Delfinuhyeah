62ba769859d4d5412d13954edabbc695e6f9d19d
Mo24-coding <mohammedbenaddi@Mohammeds-MacBook-Air.local>
2025-05-08 13:15:29 +0200

    c

--- src/Competitionresult.java
+++ src/Competitionresult.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 public class Competitionresult {
     private String disciplin;
     private double tid;
@@ -12,6 +13,35 @@ public class Competitionresult {
         this.dato = dato;
         this.medlemsnummer = medlemsnummer;
 
+    }
+    public Competitionresult(String disciplin, double tid, String stævne, int placering, String dato, int medlemsnummer){
+        this.disciplin = disciplin;
+        this.tid = tid;
+        this.stævne = stævne;
+        this.placering = placering;
+        this.dato = dato;
+        this.medlemsnummer = medlemsnummer;
+    }
+
+    public double getTid() {return tid;}
+
+    public int getPlacering() {return placering;}
+
+    public int getMedlemsnummer() {return medlemsnummer;}
+
+    public String getDisciplin() {return disciplin;}
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(medlemsnummer);
+        stringBuilder.append(";");
+        stringBuilder.append(disciplin);
+        stringBuilder.append(";");
+        stringBuilder.append(tid);
+        stringBuilder.append(";");
+        stringBuilder.append(dato);
+        return stringBuilder.toString();
     }
 
 
