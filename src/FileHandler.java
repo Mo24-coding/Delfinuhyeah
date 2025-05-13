4b665e720aeb702b534d43831b5421e412814b3f
Mo24-coding <mohammedbenaddi@Mohammeds-MacBook-Air.local>
2025-05-13 03:15:56 +0200

    c

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,3 +1,47 @@
+import java.io.*;
+import java.util.List;
+
 public class FileHandler {
-    // TIL VORES FIL
+
+    // Gemmer liste af medlemmer til fil
+    public static void saveMembers(List<Member> members, String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(members);
+            System.out.println("Medlemsdata gemt til fil: " + filename);
+        } catch (IOException e) {
+            System.out.println("Fejl ved gemning af medlemsdata: " + e.getMessage());
+        }
+    }
+
+    // Indlæser liste af medlemmer fra fil
+    @SuppressWarnings("unchecked")
+    public static List<Member> loadMembers(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (List<Member>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Fejl ved indlæsning af medlemsdata: " + e.getMessage());
+            return null;
+        }
+    }
+
+    // Gemmer liste af svømmeresultater til fil
+    public static void saveResults(List<SwimmerResult> results, String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(results);
+            System.out.println("Resultatdata gemt til fil: " + filename);
+        } catch (IOException e) {
+            System.out.println("Fejl ved gemning af resultatdata: " + e.getMessage());
+        }
+    }
+
+    // Indlæser liste af svømmeresultater fra fil
+    @SuppressWarnings("unchecked")
+    public static List<SwimmerResult> loadResults(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (List<SwimmerResult>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Fejl ved indlæsning af resultatdata: " + e.getMessage());
+            return null;
+        }
+    }
 }
